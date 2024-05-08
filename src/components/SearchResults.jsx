061037ab4addebffd85f0f8db25bff066665057a
Monishparameswaran <monishparameshwaran@gmail.com>
2024-05-09 00:08:19 +0530

    Fixed UI inconsistency added await to get rid of Old Data problem

--- src/components/SearchResults.jsx
+++ src/components/SearchResults.jsx
@@ -1,15 +1,19 @@
 import React from "react";
-
+import { Link } from "react-router-dom";
 export default function SearchResults({data}) {
     // this component is responsible for displaying the pop recommendeted book box while searching 
 
     // function handleClick(){
     //     console.log("i clicked");
     // }
-    const listItems = data.map((data) => (
-        <div className="font-normal hover:font-medium bg-gray-100 individual-recommendation-box">
-            {data}
-        </div>
+    const listItems = data.map((item) => (
+        <Link to={`/search?input=${item}`}>
+             <div className="font-normal hover:font-medium bg-gray-100 individual-recommendation-box" onClick={()=>{console.log("You will Achieve");}}>
+            {item}
+           
+            </div>     
+        </Link>
+       
     ));  
     return (
         // <div>
